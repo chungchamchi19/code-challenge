9bca2acf8e051dc15ca4085d5df806760c8aa2e8
chungchamchi <dinhchungkhtnk29@gmail.com>
2024-05-14 09:21:10 +0700

    fix: rename file in problem 3

--- src/problem3/messy.tsx
+++ src/problem3/messy.tsx
@@ -1,7 +1,6 @@
 interface WalletBalance {
   currency: string;
   amount: number;
-  blockchain: string;
 }
 interface FormattedWalletBalance {
   currency: string;
@@ -12,41 +11,64 @@ interface FormattedWalletBalance {
 interface Props extends BoxProps {
 
 }
-
-const blockchainPriorityMap: Record<string, number> = {
-  'Osmosis': 100,
-  'Ethereum': 50,
-  'Arbitrum': 30,
-  'Zilliqa': 20,
-  'Neo': 20,
-}
-
 const WalletPage: React.FC<Props> = (props: Props) => {
   const { children, ...rest } = props;
   const balances = useWalletBalances();
   const prices = usePrices();
 
+	const getPriority = (blockchain: any): number => {
+	  switch (blockchain) {
+	    case 'Osmosis':
+	      return 100
+	    case 'Ethereum':
+	      return 50
+	    case 'Arbitrum':
+	      return 30
+	    case 'Zilliqa':
+	      return 20
+	    case 'Neo':
+	      return 20
+	    default:
+	      return -99
+	  }
+	}
+
   const sortedBalances = useMemo(() => {
     return balances.filter((balance: WalletBalance) => {
-      return getPriority(balance.blockchain) !== undefined && balance.amount <= 0;
-    }).sort((lhs: WalletBalance, rhs: WalletBalance) => {
-      return getPriority(rhs.blockchain) - getPriority(lhs.blockchain);
+		  const balancePriority = getPriority(balance.blockchain);
+		  if (lhsPriority > -99) {
+		     if (balance.amount <= 0) {
+		       return true;
+		     }
+		  }
+		  return false
+		}).sort((lhs: WalletBalance, rhs: WalletBalance) => {
+			const leftPriority = getPriority(lhs.blockchain);
+		  const rightPriority = getPriority(rhs.blockchain);
+		  if (leftPriority > rightPriority) {
+		    return -1;
+		  } else if (rightPriority > leftPriority) {
+		    return 1;
+		  }
     });
-  }, [balances]);
+  }, [balances, prices]);
 
-  const getPriority = (blockchain: string): number => {
-    return blockchainPriorityMap[blockchain]
-  }
+  const formattedBalances = sortedBalances.map((balance: WalletBalance) => {
+    return {
+      ...balance,
+      formatted: balance.amount.toFixed()
+    }
+  })
 
   const rows = sortedBalances.map((balance: FormattedWalletBalance, index: number) => {
     const usdValue = prices[balance.currency] * balance.amount;
     return (
-      <WalletRow
+      <WalletRow 
         className={classes.row}
-        key={index} // should change to id if balance has id
+        key={index}
         amount={balance.amount}
         usdValue={usdValue}
-        formattedAmount={balance.amount.toFixed()}
+        formattedAmount={balance.formatted}
       />
     )
   })
